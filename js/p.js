07885ca023f5f2f24b19eaa4b82170a2515820ad
ENC6 <70848877+ENC6@users.noreply.github.com>
2020-10-01 10:49:44 +0800

    Create p.js

---
+++ js/p.js
@@ -0,0 +1,9 @@
+function check(){
+      var name=document.getElementById("name").value;
+   var pass=document.getElementById("pass").value;
+   if(name=="loongth" && pass=="666666"){
+   alert("登入成功，将自动跳转到下载页面");
+   window.document.f.action="./files/TIAN.pptx";
+   window.document.f.submit();
+   }else{
+   alert("用户名或密码错误");
